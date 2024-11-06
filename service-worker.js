33ba730ad4a745cb8bf3fd1bff1db6dd0d5c437c
DrJackHammer <kaangonel@gmail.com>
2024-11-06 21:23:04 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,24 @@
+const CACHE_NAME = 'trading-journal-v1';
+const ASSETS = [
+    '/',
+    '/index.html',
+    '/css/main.css',
+    '/js/app.js',
+    '/manifest.json'
+];
 
+// Install service worker
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(ASSETS))
+    );
+});
+
+// Fetch resources
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
+});
